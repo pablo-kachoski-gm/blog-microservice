40c485a64bcfac2c4f68e59ef1849747a3838852
PabloK <pkachoski@gmail.com>
2020-11-04 01:16:17 -0300

    Added Icons

--- client/src/posts/pages/posts/posts-list/PostListItem.jsx
+++ client/src/posts/pages/posts/posts-list/PostListItem.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Delete } from "@styled-icons/material";
 
 const Post = styled.li`
   background: linear-gradient(90deg, #d53369 0%, #daae51 100%);
@@ -7,6 +8,8 @@ const Post = styled.li`
   border-radius: 4px;
   list-style: none;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4);
+  display: flex;
+  justify-content: space-between;
 `;
 const ItemProp = styled.div`
   display: flex;
@@ -16,6 +19,19 @@ const ItemProp = styled.div`
     margin-right: 1em;
   }
 `;
+const Actions = styled.div`
+  display: flex;
+  align-items: center;
+  & > *:not(:last-child) {
+    margin-right: 0.5em;
+  }
+  & > * {
+    cursor: pointer;
+  }
+`;
+const WhiteDelete = styled(Delete)`
+  color: #d4d0d0;
+`;
 
 const PostListItem = ({ title }) => {
   return (
@@ -24,6 +40,9 @@ const PostListItem = ({ title }) => {
         <span>Title: </span>
         <span>{title}</span>
       </ItemProp>
+      <Actions>
+        <WhiteDelete size="22" />
+      </Actions>
     </Post>
   );
 };
